6a348ab7c22bb96beac2a0fd2f229bf433bd6d67
Azamatjon <azamatergasev01@gmail.com>
2024-03-22 22:08:23 +0500

    js tasks done

---
+++ main.js
@@ -0,0 +1,101 @@
+
+//  1 tasks starts nechta toq va juft son bor ekanini topadi ; 
+
+
+
+// let array = [ 1 , 3, 5,12 , 14 , 16, 22 , 5, 2, 4] 
+
+// let total_even = 0 
+// let total_odd = 0 
+
+// for(let i = 0 ; i < array.length ; i++) {
+//     if(array[i] % 2 == 0) {
+//         total_even++
+//     }else{
+//         total_odd++
+//     }
+// }
+
+
+// console.log(`Arrayda kiritilgan sonlar ichida ${total_even} ta juft sonlar mavjud `);
+// console.log(`Arrayda kiritilgan sonlar ichida ${total_odd} ta toq sonlar mavjud`);
+
+
+// 1 tasks finished 
+
+
+//  2 tasks starts juft va toq sonlar  yigindisi orasidagi farqni topadi  
+
+// let array = [ 1, 2 , 3, 5, 7, 10, 12, 8, 4,]
+
+
+// let total_even = 0 
+// let total_odd = 0 
+
+// let result = 0 
+
+// for( let i = 0 ; i < array.length ; i++) {
+    
+//     if(array[i] % 2 == 0) {
+//         total_even += array[i]
+//     }else{
+//         total_odd += array[i]
+//     }
+// }
+
+
+// console.log(`Arrayda kiritilgan sonlarni ichidagi juftini yigindisi ${total_even} tadan iborat`) 
+// console.log(`Arrayda kiritilgan sonlar ichidagi toq sonlar yigindisi ${total_odd} tadan iborat`) 
+
+
+//     result = (total_even - total_odd)
+
+
+
+
+// console.log(`Juft va toq sonlar yigindisi farqi ${result} ga teng`)
+ 
+
+// 2 tasks finished 
+
+//  4 tasks starts stringlarni aniqlaymiz  
+
+
+// let array = [ null , 32 , "lorem" , false , 23 , "text" , "tex" , "str" ]
+
+// let result = []
+
+// for(let i = 0 ; i < array.length ; i++) {
+//     if(typeof array[i] === "string") {
+//         result.push(array[i])
+//     }
+// }
+
+// console.log(`arraydagi barcha  string daty type 
+// ${result} 
+// shu yerda yigilgan`)
+
+
+// 4 task finished 
+
+
+//  6task stars juftlarni alohida arrayga olish 
+
+
+// let array = [23 , 14 , 13 , 11, 8, 22,34,11, 12 , 2, 44 , 9] 
+
+// let even = []
+
+
+// for (let i = 0 ; i < array.length ; i++) {
+//     if(array[i] % 2 == 0) {
+//         even.push(array[i])
+
+//     }
+// }
+
+// console.log(`kiritilgan array ichida ${even} shuncha juft sonlar mavjud`)
+
+
+//  6 taks finish 
+
